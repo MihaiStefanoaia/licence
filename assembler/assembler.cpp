592ac936a5dbcadfe60584aafe1531ebc00bc6da
Stefanoaia Mihai <miahistefanoaia1102@gmail.com>
2022-08-21 03:13:29 +0300

    half-ish of the core assembler is done

--- assembler/assembler.cpp
+++ assembler/assembler.cpp
@@ -10,6 +10,9 @@
 #include <set>
 
 int main(int argc, char** argv){
+    
+    char c_str_buf[256];
+
     bool finishExecution = false;
     std::regex validInput(".*\\.hra");
     std::regex validOutput(".*\\.bin");
@@ -98,7 +101,7 @@ int main(int argc, char** argv){
     std::set<std::string> labelKeys;
 
     //tokens
-    std::vector<std::string> opcodes        {"nop","mov","spc","jfg","inc","dec","not","rot","and","or","adc","sbb","mul","div","mov16","add16"};
+    std::vector<std::string> opcodes        {"nop","mov","spc","jfg","inc","dec","not","rot","and","or","adc","sbb","mul","div","mov16","add16","jmp","jeq","jlt","jle","rdp","wrp","ins","inr","push","pop","call","ret"}; //includes compiler macros and the various types of jumps
     std::vector<std::string> registers8     {"rga","rgb","rgc","rgd","rmh","rml","rsh","rsl","rbh","rbl","rph","rpl"};
     std::vector<std::string> registers16    {"rmx","rsx","rbx","rcx"};
     std::vector<std::string> memoryAccess   {"@rmx","@rsx","@rbx","@rcx"};
@@ -133,17 +136,33 @@ int main(int argc, char** argv){
             }
         } else {                                                //opcode
             for(int op = 0; op < opcodes.size(); op++){
-                if(components[0] == opcodes[op]){   //nop
-                    if(op == 0){
+                if(components[0] == opcodes[op]){   
+                    if(op == 0){ //nop
                         instructionSize = 1;
                         instructionVector[0] = 0;
 
-                    } else if(op == 1 || (op >= 8 && op <= 11)){ //mov,jfg,and,or,adc,sbb
+                    } else if(op == 2){                         //raw spc - do not allow for now
+                        std::string err("error: raw spc used at line ");
+                        itoa(line,c_str_buf,10);
+                        err += c_str_buf;
+                        err += " this is not allowed yet. use rdp, wrp, ins or inr instead, depending on your intention";
+                        errors.push_back(err);
+                        finishExecution = true;
+                    } else if(op == 3){                         //raw jfg - do not allow for now
+                        std::string err("error: raw jfg used at line ");
+                        itoa(line,c_str_buf,10);
+                        err += c_str_buf;
+                        err += " this is not allowed yet. use jmp, jeq, jlt or jle instead";
+                        errors.push_back(err);
+                        finishExecution = true;
+
+                    }else if(op == 1 || (op >= 8 && op <= 11)){ //mov,and,or,adc,sbb
                         if(components.size() != 3){
                             std::string err("error: inappropriate amount of arguments for");
                             err += opcodes[op];
-                            err += "instruction at line ";
-                            err += line;
+                            err += " instruction at line ";
+                            itoa(line,c_str_buf,10);
+                            err += c_str_buf;
                             errors.push_back(err);
                             finishExecution = true;
                         }
@@ -164,7 +183,7 @@ int main(int argc, char** argv){
                         }
                         if(!found){
                             for(int i = 0; i < memoryAccess.size(); i++){
-                                if(components[1] == memoryAccess[i]){
+                                if(components[1] == memoryAccess[i] && op == 1){ //memory access is valid as left operand only on mov
                                     instructionVector[0] |= 2 << 4;
                                     instructionVector[1] |= i;
                                     memoryAccesses++;
@@ -174,7 +193,8 @@ int main(int argc, char** argv){
                         }
                         if(!found){
                             std::string err("error: invalid left operand at line ");
-                            err += line;
+                            itoa(line,c_str_buf,10);
+                            err += c_str_buf;
                             errors.push_back(err);
                             finishExecution = true;
                         }
@@ -206,7 +226,8 @@ int main(int argc, char** argv){
                                     std::string err("warning: immediate is out of bounds (");
                                     err += imm;
                                     err += " is not in [0,255] or [-128,127]) at line "; 
-                                    err += line;
+                                    itoa(line,c_str_buf,10);
+                                    err += c_str_buf;
                                     errors.push_back(err);
                                 }
                                 instructionSize = 3;
@@ -217,59 +238,221 @@ int main(int argc, char** argv){
                         }
                         if(!found){
                             std::string err("error: invalid right operand at line ");
-                            err += line;
+                            itoa(line,c_str_buf,10);
+                            err += c_str_buf;
                             errors.push_back(err);
                             finishExecution = true;
                         }
                         if(memoryAccesses == 2){
                             std::string err("error: too many memory accesses at line ");
-                            err += line;
+                            itoa(line,c_str_buf,10);
+                            err += c_str_buf;
                             errors.push_back(err);
                             finishExecution = true;
                         }
-                    } else if (op >= 4 || op <= 6)  //inc, dec, not
-                    {
-                        if(components.size() != 3){
+                    } else if (op >= 4 && op <= 6){ //inc, dec, not
+                        if(components.size() != 2){
                             std::string err("error: inappropriate amount of arguments for");
                             err += opcodes[op];
-                            err += "instruction at line ";
-                            err += line;
+                            err += " instruction at line ";
+                            itoa(line,c_str_buf,10);
+                            err += c_str_buf;
                             errors.push_back(err);
                             finishExecution = true;
                         }
 
 
                         int memoryAccesses = 0;
-                        instructionSize = 2;
+                        instructionSize = 1;
                         instructionVector[0] |= op;
 
-                        bool found = false; //evaluate left operand
+                        bool found = false; //evaluate operand
 
                         for(int i = 0; i < registers8.size(); i++){
                             if(components[1] == registers8[i]){
-                                instructionVector[0] |= 1 << 4;
-                                instructionVector[1] |= i+4;
+                                instructionVector[0] |= i+4 << 4;
                                 found = true;
                             }
                         }
+
                         if(!found){
-                            for(int i = 0; i < memoryAccess.size(); i++){
-                                if(components[1] == memoryAccess[i]){
-                                    instructionVector[0] |= 2 << 4;
-                                    instructionVector[1] |= i;
-                                    memoryAccesses++;
-                                    found = true;
-                                }
+                            std::string err("error: invalid operand at line ");
+                            itoa(line,c_str_buf,10);
+                            err += c_str_buf;
+                            errors.push_back(err);
+                            finishExecution = true;
+                        }
+                    } else if (op == 7){ //rot
+                        if(components.size() != 3){
+                            std::string err("error: inappropriate amount of arguments for ");
+                            err += opcodes[op];
+                            err += "instruction at line ";
+                            itoa(line,c_str_buf,10);
+                            err += c_str_buf;
+                            errors.push_back(err);
+                            finishExecution = true;
+                        }
+                        bool found = false;
+                        instructionVector[0] |= op;
+                        instructionSize = 2;
+                        
+                        for(int i = 0; i < registers8.size(); i++){
+                            if(components[1] == registers8[i]){
+                                instructionVector[0] |= i+4 << 4;
+                                found = true;
                             }
                         }
+
                         if(!found){
                             std::string err("error: invalid left operand at line ");
-                            err += line;
+                            itoa(line,c_str_buf,10);
+                            err += c_str_buf;
+                            errors.push_back(err);
+                            finishExecution = true;
+                        }
+
+                        found = false;
+                        
+
+                        if(std::regex_match(components[2],std::regex("(-?[1-9][0-9]*)|0"))){ //check for immediate
+                            int imm = atoi(components[2].c_str());
+                            std::cout<<imm<<'\n';
+                            if(imm > 255 | imm < -128){
+                                std::string err("warning: immediate is out of bounds (");
+                                err += imm;
+                                err += " is not in [0,255] or [-128,127]) at line "; 
+                                itoa(line,c_str_buf,10);
+                                err += c_str_buf;
+                                errors.push_back(err);
+                            }
+                            instructionSize = 2;
+                            instructionVector[1]  = imm % 256;
+                            found = true;
+                        }
+
+                        if(!found){
+                            std::string err("error: invalid right operand at line ");
+                            itoa(line,c_str_buf,10);
+                            err += c_str_buf;
+                            errors.push_back(err);
+                            finishExecution = true;
+                        }
+                    } else if(op == 12 || op == 13){
+                        if(components.size() != 1){
+                            std::string err("error: inappropriate amount of arguments for ");
+                            err += opcodes[op];
+                            err += "instruction at line ";
+                            itoa(line,c_str_buf,10);
+                            err += c_str_buf;
+                            errors.push_back(err);
+                            finishExecution = true;
+                        }
+                        instructionSize = 1;
+                        instructionVector[0] |= op;
+                    } else if(op == 14){
+                        if(components.size() != 3){
+                            std::string err("error: inappropriate amount of arguments for ");
+                            err += opcodes[op];
+                            err += "instruction at line ";
+                            itoa(line,c_str_buf,10);
+                            err += c_str_buf;
+                            errors.push_back(err);
+                            finishExecution = true;
+                        }
+
+                        bool found = false;
+                        
+                        instructionSize = 1;
+                        instructionVector[0] |= op;
+
+                        for(int i = 0; i < registers16.size(); i++){
+                            if(components[1] == registers16[i]){
+                                instructionVector[0] |= i << 4;
+                                found = true;
+                            }
+                        }
+                        
+                        if(!found){
+                            std::string err("error: invalid left operand at line ");
+                            itoa(line,c_str_buf,10);
+                            err += c_str_buf;
+                            errors.push_back(err);
+                            finishExecution = true;
+                        }
+
+                        bool found = false;
+
+                        for(int i = 0; i < registers16.size(); i++){
+                            if(components[1] == registers16[i]){
+                                instructionVector[0] |= i << 6;
+                                found = true;
+                            }
+                        }
+
+                        if(!found){
+                            std::string err("error: invalid right operand at line ");
+                            itoa(line,c_str_buf,10);
+                            err += c_str_buf;
+                            errors.push_back(err);
+                            finishExecution = true;
+                        }
+
+
+                    } else if(op == 15){
+                        if(components.size() != 3){
+                            std::string err("error: inappropriate amount of arguments for ");
+                            err += opcodes[op];
+                            err += "instruction at line ";
+                            itoa(line,c_str_buf,10);
+                            err += c_str_buf;
+                            errors.push_back(err);
+                            finishExecution = true;
+                        }
+                        bool found = false;
+                        
+                        instructionSize = 2;
+                        instructionVector[0] |= op;
+
+                        for(int i = 0; i < registers16.size(); i++){
+                            if(components[1] == registers16[i]){
+                                instructionVector[0] |= i << 4;
+                                found = true;
+                            }
+                        }
+
+                        if(!found){
+                            std::string err("error: invalid left operand at line ");
+                            itoa(line,c_str_buf,10);
+                            err += c_str_buf;
+                            errors.push_back(err);
+                            finishExecution = true;
+                        }
+                        
+                        found = false;
+
+                        if(std::regex_match(components[2],std::regex("(-?[1-9][0-9]*)|0"))){ //check for immediate
+                            int imm = atoi(components[2].c_str());
+                            std::cout<<imm<<'\n';
+                            if(imm > 255 | imm < -128){
+                                std::string err("warning: immediate is out of bounds (");
+                                err += imm;
+                                err += " is not in [0,255] or [-128,127]) at line "; 
+                                itoa(line,c_str_buf,10);
+                                err += c_str_buf;
+                                errors.push_back(err);
+                            }
+                            instructionVector[1]  = imm % 256;
+                            found = true;
+                        }
+
+                        if(!found){
+                            std::string err("error: invalid right operand at line ");
+                            itoa(line,c_str_buf,10);
+                            err += c_str_buf;
                             errors.push_back(err);
                             finishExecution = true;
                         }
                     }
-                    
                 }
             }
         }
